0fdfdf6c9d877f3f42e827173c9ebaed9f792e10
Sébastien Labbé <slabqc@gmail.com>
2024-06-18 15:56:20 +0200

    ring -> base_ring to fix deprecation warning

--- slabbe/matrices.py
+++ slabbe/matrices.py
@@ -51,17 +51,17 @@ def projection_matrix(dim_from=3, dim_to=2):
     from sage.rings.real_mpfr import RR
     sqrt3 = sqrt(3)
     if dim_from == 3 and dim_to == 2:
-        return matrix(2,[-sqrt3,sqrt3,0,-1,-1,2],ring=RR)/2
+        return matrix(2,[-sqrt3,sqrt3,0,-1,-1,2],base_ring=RR)/2
     elif dim_from == 2 and dim_to == 3:
-        return matrix(3,[-sqrt3,-1,sqrt3,-1,0,2],ring=RR)/3
+        return matrix(3,[-sqrt3,-1,sqrt3,-1,0,2],base_ring=RR)/3
     elif dim_from == 4 and dim_to == 2:
-        return matrix(2,[-sqrt3,sqrt3,0,1,-1,-1,2,0],ring=RR)/2
+        return matrix(2,[-sqrt3,sqrt3,0,1,-1,-1,2,0],base_ring=RR)/2
     elif dim_from == 4 and dim_to == 3:
         sqrt2 = sqrt(2)
         return matrix([(1,-1,0,0), 
                        (0,0,1,-1),
                        (-1/sqrt2,-1/sqrt2,1/sqrt2,1/sqrt2)],
-                       ring=RR)
+                       base_ring=RR)
     else:
         s = "for input dim_from={} and dim_to={}"
         raise NotImplementedError(s.format(dim_from, dim_to))
