e3b1802537260518ae5d9cdec5270bb969abce32
Sébastien Labbé <slabqc@gmail.com>
2025-02-12 10:05:38 +0100

    ajout Cyrenaic C&P

--- slabbe/cut_and_project_scheme.py
+++ slabbe/cut_and_project_scheme.py
@@ -1122,8 +1122,8 @@ class CutAndProjectSchemeGenerator():
         z = polygen(QQ, 'z')
         K = NumberField(z**2-z-1, 'phi', embedding=RR(1.6))
         phi = K.gen()
-        pi = matrix([[1, ~phi, 0, 0], [0, 0, 1, ~phi]])
-        pi_int = matrix([[-~phi, 1, 0, 0], [0, 0, -~phi, 1]])
+        pi = matrix(K, [[1, ~phi, 0, 0], [0, 0, 1, ~phi]])
+        pi_int = matrix(K, [[-~phi, 1, 0, 0], [0, 0, -~phi, 1]])
         return CutAndProjectScheme(K, pi, pi_int)
 
     def Penrose(self):
@@ -1179,7 +1179,7 @@ class CutAndProjectSchemeGenerator():
         z = polygen(QQ, 'z')
         K = NumberField(z**2-z-1, 'phi', embedding=RR(1.6))
         phi = K.gen()
-        E = matrix([[-1,0,phi,phi], [0,1,phi,1]])
+        E = matrix(K, [[-1,0,phi,phi], [0,1,phi,1]])
         return CutAndProjectScheme.from_slope(E)
 
     def AmmannBeenker(self):
@@ -1214,7 +1214,42 @@ class CutAndProjectSchemeGenerator():
         z = polygen(QQ, 'z')
         K = NumberField(z**2-2, 'sqrt2', embedding=RR(1.4))
         sqrt2 = K.gen()
-        E = matrix([[sqrt2,1,0,-1], [0,1,sqrt2,1]])
+        E = matrix(K, [[sqrt2,1,0,-1], [0,1,sqrt2,1]])
+        return CutAndProjectScheme.from_slope(E)
+
+    def Cyrenaic(self):
+        r"""
+        Return the Cyrenaic cut and proect scheme
+
+        The choice of the matrix E whose rows generate the slope, that is,
+        the kernel of the pi_int projection, is made acording to
+        Carole Porrier's code available at https://github.com/cporrier/Cyrenaic
+
+        EXAMPLES::
+
+            sage: from slabbe import cut_and_project_schemes
+            sage: cut_and_project_schemes.Cyrenaic()
+            4-to-2 cut and project scheme
+
+        ::
+
+            sage: c = cut_and_project_schemes.Cyrenaic()
+            sage: m = c.canonical_model_set(shift=vector((1,1,1,1))/100)
+            sage: W = polytopes.hypercube(2, intervals=[(-10,10), (-10,10)])
+            sage: G = m.plot_in_physical_space(W)      # long time (1s)
+            sage: G.show(aspect_ratio=1, figsize=20)   # long time
+
+        """
+        from sage.rings.rational_field import QQ
+        from sage.rings.real_mpfr import RR
+        from sage.rings.polynomial.polynomial_ring import polygen
+        from sage.rings.number_field.number_field import NumberField
+        from sage.matrix.constructor import matrix
+
+        z = polygen(QQ, 'z')
+        K = NumberField(z**2-3, 'sqrt3', embedding=RR(1.7))
+        sqrt3 = K.gen()
+        E = matrix(K, [[sqrt3,0,1,1],[1,sqrt3-1,-1,1]])
         return CutAndProjectScheme.from_slope(E)
 
 cut_and_project_schemes = CutAndProjectSchemeGenerator()
