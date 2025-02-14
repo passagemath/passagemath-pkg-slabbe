4c383b923a09c4a15f0af5c9a1677cd9beb3c235
Sébastien Labbé <slabqc@gmail.com>
2025-02-14 11:34:35 +0100

    added arguments to induced transformation

--- slabbe/polyhedron_exchange_transformation.py
+++ slabbe/polyhedron_exchange_transformation.py
@@ -534,7 +534,7 @@ class PolyhedronExchangeTransformation(PiecewiseAffineTransformation):
         trans_dict = {key:t for (key,t) in self.translations().items()}
         return PolyhedronExchangeTransformation(P, trans_dict)
 
-    def induced_transformation(self, ieq):
+    def induced_transformation(self, ieq, ignore_volume=0, verbose=False):
         r"""
         Return the induced transformation on the domain.
 
@@ -542,6 +542,11 @@ class PolyhedronExchangeTransformation(PiecewiseAffineTransformation):
 
         - ``ieq`` -- list, an inequality. An entry equal to "[-1,7,3,4]"
           represents the inequality 7x_1+3x_2+4x_3>= 1.
+        - ``ignore_volume`` -- real (optional:``0``), stop the while loop if
+          the volume of what's not yet returned is less than the given
+          threshold
+        - ``verbose`` -- bool (optional:``False``), print verbose
+          information
 
         OUTPUT:
 
@@ -568,7 +573,7 @@ class PolyhedronExchangeTransformation(PiecewiseAffineTransformation):
             {0: [0], 1: [0, 1], 2: [0, 0, 1]}
 
         """
-        newP, sub = self.induced_partition(ieq)
+        newP, sub = self.induced_partition(ieq, ignore_volume=ignore_volume, verbose=verbose)
         T = self.translations()
         newT = {a:sum(T[b] for b in sub[a]) for a in sub}
         return PolyhedronExchangeTransformation(newP, newT), sub
